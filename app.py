4da10ef61583a64a1ff91a0f4633aecc4831afe2
Lucyjpjones <lucyjpjones@gmail.com>
2020-10-24 13:38:41 +0000

    homepage html added to index.html and app.py amended to connect to homepage

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,10 +19,9 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_recipes")
-def get_recipes():
-    recipes = mongo.db.recipes.find()
-    return render_template("recipes.html", recipes=recipes)
+@app.route("/homepage")
+def homepage():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
